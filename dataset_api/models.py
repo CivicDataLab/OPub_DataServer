96a448e33fd707e059c89049a9753ebeac51da0c
Deepthi Chand <eopoxf@gmail.com>
2022-08-23 05:02:05 +0530

    Update models

--- dataset_api/models.py
+++ dataset_api/models.py
@@ -9,10 +9,10 @@ def _resource_directory_path(resource, filename):
     Create a directory path to upload the resources.
 
     """
-    dataset_name = slugify(resource.dataset.title)
-    resource_name = slugify(resource.title)
+    dataset_name = resource.dataset.title
+    resource_name = resource.title
     _, extension = os.path.splitext(filename)
-    return f"resources/{dataset_name}/{resource_name}/{extension}"
+    return f"resources/{dataset_name}/{resource_name}/{extension[1:]}/{filename}"
 
 
 class Organization(models.Model):
@@ -21,7 +21,7 @@ class Organization(models.Model):
     issued = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     homepage = models.URLField(blank=True)
-    contact_email = models.EmailField()
+    contact_email = models.EmailField(blank=True)
 
 
 class Catalog(models.Model):
@@ -47,7 +47,8 @@ class Resource(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     issued = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField()
+    modified = models.DateTimeField(auto_now=True)
     remote_url = models.URLField(blank=True)
+    format = models.CharField(max_length=15)
     file = models.FileField(upload_to=_resource_directory_path, blank=True)
     dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
