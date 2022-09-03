a38bc2557e7756c3f313caa8dff1e68e110ced72
Abhinav <a.kumar2102@gmail.com>
2022-09-03 17:05:39 +0530

    ADD- user module

--- DatasetServer/schema.py
+++ DatasetServer/schema.py
@@ -3,21 +3,44 @@ import dataset_api.dataset_schema
 import dataset_api.organization_schema
 import dataset_api.catalog_schema
 import dataset_api.resource_schema
+from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
 
 
 class Query(dataset_api.dataset_schema.Query, dataset_api.organization_schema.Query, dataset_api.catalog_schema.Query,
-            dataset_api.resource_schema.Query,
+            dataset_api.resource_schema.Query, UserQuery, MeQuery, 
             graphene.ObjectType):
     pass
 
 
-class Mutation(graphene.ObjectType):
+class AuthMutation(graphene.ObjectType):
+   register = mutations.Register.Field()
+   verify_account = mutations.VerifyAccount.Field()
+   token_auth = mutations.ObtainJSONWebToken.Field()
+   refresh_token = mutations.RefreshToken.Field()
+   revoke_token = mutations.RevokeToken.Field()
+   
+   resend_activation_email = mutations.ResendActivationEmail.Field()
+   send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+   password_reset = mutations.PasswordReset.Field()
+   password_change = mutations.PasswordChange.Field()
+   archive_account = mutations.ArchiveAccount.Field()
+   delete_account = mutations.DeleteAccount.Field()
+   update_account = mutations.UpdateAccount.Field()
+   send_secondary_email_activation = mutations.SendSecondaryEmailActivation.Field()
+   verify_secondary_email = mutations.VerifySecondaryEmail.Field()
+   swap_emails = mutations.SwapEmails.Field()
+
+
+class Mutation(AuthMutation, graphene.ObjectType):
     create_dataset = dataset_api.dataset_schema.CreateDataset.Field()
     update_dataset = dataset_api.dataset_schema.UpdateDataset.Field()
     create_resource = dataset_api.resource_schema.CreateResource.Field()
     update_resource = dataset_api.resource_schema.UpdateResource.Field()
     create_catalog = dataset_api.catalog_schema.CreateCatalog.Field()
     create_organization = dataset_api.organization_schema.CreateOrganization.Field()
+    
+
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation, auto_camelcase=False)
