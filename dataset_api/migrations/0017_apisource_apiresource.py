87b1c9c6baf618f2e1c4799c490227f78c044ca5
Deepthi Chand <eopoxf@gmail.com>
2022-09-13 03:53:56 +0530

    remove unwanted migrations

--- dataset_api/migrations/0017_apisource_apiresource.py
+++ dataset_api/migrations/0017_apisource_apiresource.py
@@ -26,18 +26,5 @@ class Migration(migrations.Migration):
                 ('auth_credentials', models.JSONField(blank=True, null=True)),
                 ('auth_token', models.CharField(blank=True, max_length=200, null=True)),
             ],
-        ),
-        migrations.CreateModel(
-            name='APIResource',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('description', models.CharField(max_length=500)),
-                ('issued', models.DateTimeField(auto_now_add=True)),
-                ('modified', models.DateTimeField(auto_now=True)),
-                ('status', models.CharField(default='Draft', max_length=50)),
-                ('masked_fields', django.contrib.postgres.fields.ArrayField(base_field=models.CharField(blank=True, max_length=10), blank=True, null=True, size=None)),
-                ('dataset', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='dataset_api.dataset')),
-            ],
-        ),
+        )
     ]
