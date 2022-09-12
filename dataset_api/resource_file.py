704e7062fe62262ec84925b8d9debefca8415ffb
Deepthi Chand <eopoxf@gmail.com>
2022-09-12 05:38:51 +0530

    Add simple resource file download api

---
+++ dataset_api/resource_file.py
@@ -0,0 +1,15 @@
+import os
+
+from django.http import HttpResponse
+import mimetypes
+from dataset_api.models import Resource
+
+
+def download(request, resource_id):
+    resource = Resource.objects.get(pk=resource_id)
+    file_path = resource.file.path
+    if len(file_path):
+        mime_type = mimetypes.guess_type(file_path)
+        response = HttpResponse(resource.file, content_type=mime_type)
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
+        return response
