829f3eaac617436f633ae47e0f610256c5efd3e6
Deepthi Chand <eopoxf@gmail.com>
2022-09-13 03:44:39 +0530

    add api resource

--- dataset_api/models.py
+++ dataset_api/models.py
@@ -94,5 +94,12 @@ class APISource(models.Model):
     auth_token = models.CharField(blank=True, null=True, max_length=200)
 
 
-
+class APIResource(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    issued = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, default='Draft')
+    masked_fields = ArrayField(models.CharField(max_length=10, blank=True), blank=True, null=True)
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
 
