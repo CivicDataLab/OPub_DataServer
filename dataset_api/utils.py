9ff52e304ecb5a1ea4cee2538cd6627396ee8321
Deepthi-Chand <eopoxf@gmail.com>
2022-09-17 17:26:26 +0530

    Add file format mapping utility

---
+++ dataset_api/utils.py
@@ -0,0 +1,10 @@
+FORMAT_MAPPING = {
+    "text/csv": "CSV",
+    "application/json": "JSON",
+    "application/msword": "DOC",
+    "application/doc": "DOC",
+    "application/ms-doc": "DOC",
+    "application/pdf": "PDF",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "DOCX",
+    "application/xml": "XML"
+}
