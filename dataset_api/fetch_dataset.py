38296207e3e179c424cbe4ff52aae58f8a643f2b
Deepthi Chand <eopoxf@gmail.com>
2022-09-20 05:14:23 +0530

    Handle index errors

--- dataset_api/fetch_dataset.py
+++ dataset_api/fetch_dataset.py
@@ -126,8 +126,14 @@ class ApproveRejectDataRequest(graphene.Mutation, Output):
             data_request_instance.status = data_request.status
             data_request_instance.remark = data_request.remark
         data_request_instance.save()
-        resource = data_request_instance.resource.all()[0]
-        api_resource = data_request_instance.api_resource.all()[0]
+        try:
+            resource = data_request_instance.resource.all()[0]
+        except IndexError:
+            resource = None
+        try:
+            api_resource = data_request_instance.api_resource.all()[0]
+        except IndexError:
+            api_resource = None
         if api_resource and data_request.status is StatusType.APPROVED:
             url = f"https://pipeline.ndp.civicdatalab.in/transformer/api_source_query?api_source_id={api_resource.id}&request_id={data_request.id}"
             payload = {}
