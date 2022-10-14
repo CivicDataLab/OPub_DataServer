9ffdd33bc2803747cb20e55e0c476c1c4756bc7c
ArcD7 <pokemongochamp47@gmail.com>
2022-10-14 15:32:21 +0530

    Removed print.

--- dataset_api/organization_schema.py
+++ dataset_api/organization_schema.py
@@ -62,9 +62,7 @@ class UpdateOrganization(graphene.Mutation):
     @staticmethod
     def mutate(root, info, organization_data: OrganizationInput = None):
         organization_instance = Organization.objects.get(id=organization_data.id)
-        print(organization_instance)
         if organization_instance:
-            print(organization_data.title)
             organization_instance.title=organization_data.title
             organization_instance.description=organization_data.description
             organization_instance.logo=organization_data.logo
