317bfba3a743ea8e484788ec87771be7a6788b6c
ArcD7 <pokemongochamp47@gmail.com>
2022-10-17 12:58:52 +0530

    Added remote url to file details schema.

--- dataset_api/resource_schema.py
+++ dataset_api/resource_schema.py
@@ -238,6 +238,7 @@ def _create_update_file_details(resource_instance, attribute):
     except FileDetails.DoesNotExist as e:
         file_detail_object = FileDetails(resource=resource_instance)
     file_detail_object.file = attribute.file
+    file_detail_object.remote_url = attribute.remote_url
     file_detail_object.save()
     file_format = attribute.format
     if attribute.format and attribute.format == "":
