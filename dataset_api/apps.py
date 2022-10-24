02be0d5efc24f576b7bef85a6229c29dfaa25fd1
Deepthi Chand <eopoxf@gmail.com>
2022-10-24 12:57:37 +0530

    register essential models for activity log

--- dataset_api/apps.py
+++ dataset_api/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class DatasetApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dataset_api'
+
+    def ready(self):
+        from activity_log.registry import register
+        from dataset_api.models import Dataset, Resource, Organization
+        register(Dataset)
+        register(Resource)
+        register(Organization)
