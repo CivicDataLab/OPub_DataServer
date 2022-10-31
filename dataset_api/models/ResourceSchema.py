0c36800899c2b627cfa0b85e5a734bbb24ca96aa
Deepthi Chand <eopoxf@gmail.com>
2022-10-31 21:26:41 +0530

    update constraits and deleteoptions for resource schema

--- dataset_api/models/ResourceSchema.py
+++ dataset_api/models/ResourceSchema.py
@@ -5,18 +5,18 @@ from dataset_api.models.Resource import Resource
 
 class ResourceSchema(models.Model):
     class Meta:
-        unique_together = ('id', 'key')
+        unique_together = ('resource', 'key')
 
     key = models.CharField(max_length=100)
     format = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
-    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.PROTECT)
     parent = models.ForeignKey(
         "self",
         unique=False,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         related_name="parent_field",
     )
     array_field = models.OneToOneField(
@@ -24,6 +24,6 @@ class ResourceSchema(models.Model):
         unique=False,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         related_name="array_item",
     )
