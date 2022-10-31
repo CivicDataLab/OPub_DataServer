119221724f0cbb43d92ea89c547d28db9fada1ae
ArcD7 <pokemongochamp47@gmail.com>
2022-10-31 22:50:20 +0530

    Added resolver for sector stats within Sector Type

--- dataset_api/sector_schema.py
+++ dataset_api/sector_schema.py
@@ -7,41 +7,44 @@ from .models import Sector, Dataset, Catalog
 
 
 class SectorType(DjangoObjectType):
+    dataset_count = graphene.Int()
+    api_count = graphene.Int()
+    organization_count = graphene.Int()
+
     class Meta:
         model = Sector
         fields = "__all__"
 
+    def resolve_dataset_count(self, info):
+        try:
+            dataset_count = Sector.objects.get(pk=self.id).dataset_set.count()
+            return dataset_count
+        except Sector.DoesNotExist as e:
+            return None
 
-class SectorStatsType(graphene.ObjectType):
-    dataset_count = graphene.Int()
-    api_count = graphene.Int()
-    organization_count = graphene.Int()
+    def resolve_api_count(self, info):
+        api_count = Dataset.objects.filter(
+            Q(dataset_type__exact="API"), Q(sector__pk=self.id)
+        ).count()
+        return api_count
+
+    def resolve_organization_count(self, info):
+        dataset_obj = Dataset.objects.filter(sector__pk=self.id)
+        org_list = []
+        for catalog in dataset_obj:
+            org_list.append(Catalog.objects.get(id=catalog.catalog_id).organization.id)
+        return len(set(org_list))
 
 
 class Query(graphene.ObjectType):
     all_sector = graphene.List(SectorType)
     sector = graphene.Field(SectorType, sector_id=graphene.Int())
-    sector_stat = graphene.Field(SectorStatsType, sector_id=graphene.Int())
 
     def resolve_all_sector(self, info, **kwargs):
         return Sector.objects.all()
 
     def resolve_sector(self, info, sector_id):
         return Sector.objects.get(pk=sector_id)
-
-    def resolve_sector_stat(self, info, sector_id):
-        dataset = Sector.objects.get(pk=sector_id).dataset_set.count()
-        api_count = Dataset.objects.filter(
-            Q(dataset_type__exact="API"), Q(sector__pk=sector_id)
-        ).count()
-        dataset_obj = Dataset.objects.filter(sector__pk=sector_id)
-        org_list = []
-        for catalog in dataset_obj:
-            org_list.append(Catalog.objects.get(id=catalog.catalog_id).organization.id)
-        org_count = len(set(org_list))
-        return SectorStatsType(
-            dataset_count=dataset, api_count=api_count, organization_count=org_count
-        )
 
 
 class SectorInput(graphene.InputObjectType):
