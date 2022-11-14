e25f83ab7d4323b88d8f93b15fc02f967393f325
ArcD7 <pokemongochamp47@gmail.com>
2022-11-14 16:39:29 +0530

    No limit check for OPEN data access models.

--- dataset_api/ratelimits.py
+++ dataset_api/ratelimits.py
@@ -6,53 +6,65 @@ def user_key(group, request):
     request_id = request.path.split("/")[3]
     data_request_instance = DataRequest.objects.get(pk=request_id)
     user = data_request_instance.user
-    dam_id = data_request_instance.dataset_access_model_request.access_model.data_access_model
-    return user + "||" + str(dam_id.id)
+    dam = (
+        data_request_instance.dataset_access_model_request.access_model.data_access_model
+    )
+    # TODO: Handle for OPEN data access models.
+    # if dam.type == "OPEN":
+    #     user = "Unlimited"
+    return user + "||" + str(dam.id)
 
 
 def rate_per_user(group, request):
     request_id = request.path.split("/")[3]
     data_request_instance = DataRequest.objects.get(pk=request_id)
-    rate_limit = (
-        data_request_instance.dataset_access_model_request.access_model.data_access_model.rate_limit
+    dam = (
+        data_request_instance.dataset_access_model_request.access_model.data_access_model
     )
-    rate_limit_unit = (
-        data_request_instance.dataset_access_model_request.access_model.data_access_model.rate_limit_unit
-    )
-    if rate_limit_unit == "SECOND":
-        return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
-    elif rate_limit_unit == "MINUTE":
-        return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
-    elif rate_limit_unit == "HOUR":
-        return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
-    elif rate_limit_unit == "DAY":
-        return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
-    elif rate_limit_unit == "WEEK":
-        return str(rate_limit) + "/7d"
-    elif rate_limit_unit == "MONTH":
-        return str(rate_limit) + "/30d"
-    elif rate_limit_unit == "QUARTER":
-        return str(rate_limit) + "/92d"
+    rate_limit = dam.rate_limit
+    rate_limit_unit = dam.rate_limit_unit
+
+    # TODO: Handle for OPEN data access models.
+    if dam.type == "OPEN":
+        # user = "Unlimited"
+        return None  # No rate limit -- UNLIMITED usage.
     else:
-        return str(rate_limit) + "/365d"
+        if rate_limit_unit == "SECOND":
+            return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
+        elif rate_limit_unit == "MINUTE":
+            return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
+        elif rate_limit_unit == "HOUR":
+            return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
+        elif rate_limit_unit == "DAY":
+            return str(rate_limit) + "/" + rate_limit_unit.lower()[0]
+        elif rate_limit_unit == "WEEK":
+            return str(rate_limit) + "/7d"
+        elif rate_limit_unit == "MONTH":
+            return str(rate_limit) + "/30d"
+        elif rate_limit_unit == "QUARTER":
+            return str(rate_limit) + "/92d"
+        else:
+            return str(rate_limit) + "/365d"
 
 
 def quota_per_user(group, request):
     request_id = request.path.split("/")[3]
     data_request_instance = DataRequest.objects.get(pk=request_id)
-    quota_limit = (
-        data_request_instance.dataset_access_model_request.access_model.data_access_model.subscription_quota
+    dam = (
+        data_request_instance.dataset_access_model_request.access_model.data_access_model
     )
-    quota_limit_unit = (
-        data_request_instance.dataset_access_model_request.access_model.data_access_model.subscription_quota_unit
-    )
-    if quota_limit_unit == SubscriptionUnits.DAILY:
-        return str(quota_limit) + "/" + quota_limit_unit.lower()[0]
-    elif quota_limit_unit == SubscriptionUnits.WEEKLY:
-        return str(quota_limit) + "/7d"
-    elif quota_limit_unit == SubscriptionUnits.MONTHLY:
-        return str(quota_limit) + "/30d"
-    elif quota_limit_unit == SubscriptionUnits.QUARTERLY:
-        return str(quota_limit) + "/92d"
+    quota_limit = dam.subscription_quota
+    quota_limit_unit = dam.subscription_quota_unit
+    if dam.type == "OPEN":
+        return None  # No rate limit -- UNLIMITED usage.
     else:
-        return str(quota_limit) + "/365d"
+        if quota_limit_unit == SubscriptionUnits.DAILY:
+            return str(quota_limit) + "/" + quota_limit_unit.lower()[0]
+        elif quota_limit_unit == SubscriptionUnits.WEEKLY:
+            return str(quota_limit) + "/7d"
+        elif quota_limit_unit == SubscriptionUnits.MONTHLY:
+            return str(quota_limit) + "/30d"
+        elif quota_limit_unit == SubscriptionUnits.QUARTERLY:
+            return str(quota_limit) + "/92d"
+        else:
+            return str(quota_limit) + "/365d"
