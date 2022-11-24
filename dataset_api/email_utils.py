e61d906fb975503039d69e1a834e3fc70fde29cf
ArcD7 <pokemongochamp47@gmail.com>
2022-11-25 03:16:12 +0530

    Email notification on dataset approval

---
+++ dataset_api/email_utils.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+email_url = "https://mailer.idp.civicdatalab.in/send-email/backgroundtasks"
+
+
+def dataset_approval_notif(username, dataset_id, org_id):
+    body = {
+        "actor": username,
+        "action": "Approved",
+        "tgt_obj": dataset_id,
+        "tgt_group": "Dataset",
+        "extras": {"tgt_org": org_id},
+    }
+    headers = {}
+    response = requests.request("POST", email_url, data=body, headers=headers)
+    response_json = response.text
+    return response_json
