3d68fcf3b3d7094f1aa77f266b8c53fe6a1c7142
Deepthi Chand <eopoxf@gmail.com>
2022-11-27 02:48:25 +0530

    add specifications to data request parameter to data request foreignkey

--- dataset_api/models/DataRequestParameter.py
+++ dataset_api/models/DataRequestParameter.py
@@ -5,5 +5,6 @@ from dataset_api.models import APIParameter, DataRequest
 
 class DataRequestParameter(models.Model):
     api_parameter = models.ForeignKey(APIParameter, on_delete=models.PROTECT)
-    data_request = models.ForeignKey(DataRequest, on_delete=models.PROTECT)
+    data_request = models.ForeignKey(DataRequest, on_delete=models.PROTECT, to_field='id',
+                                     related_name="datarequestpatameters")
     value = models.CharField(max_length=100)
