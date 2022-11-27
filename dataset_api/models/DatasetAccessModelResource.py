557f49943533dc1e5bdafff370982c9a8ff0f8a6
ArcD7 <pokemongochamp47@gmail.com>
2022-11-27 23:28:54 +0530

    Dataset access model resource many to many field

--- dataset_api/models/DatasetAccessModelResource.py
+++ dataset_api/models/DatasetAccessModelResource.py
@@ -3,10 +3,10 @@ from django.db import models
 
 from dataset_api.models.DatasetAccessModel import DatasetAccessModel
 from dataset_api.models.Resource import Resource
-
+from dataset_api.models.ResourceSchema import ResourceSchema
 
 class DatasetAccessModelResource(models.Model):
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     # Make this one to many field
-    fields = ArrayField(models.CharField(max_length=25, blank=False), blank=False, null=False)
+    fields = models.ManyToManyField(ResourceSchema)
     dataset_access_model = models.ForeignKey(DatasetAccessModel, on_delete=models.CASCADE, default="")
