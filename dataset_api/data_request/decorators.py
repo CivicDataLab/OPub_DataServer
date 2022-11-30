04904050de2593bc748dd1e436fb0fe4cae2a607
ArcD7 <pokemongochamp47@gmail.com>
2022-11-30 12:16:24 +0530

    Removed validity for OPEN data access model

--- dataset_api/data_request/decorators.py
+++ dataset_api/data_request/decorators.py
@@ -12,40 +12,45 @@ def dam_request_validity(func):
         dataset_access_request_status = (
             data_request_instance.dataset_access_model_request.status
         )
-        # Check if the dataset model request is APPROVED
-        if dataset_access_request_status == "APPROVED":
-            validation = (
-                data_request_instance.dataset_access_model_request.access_model.data_access_model.validation
-            )
-            validation_unit = (
-                data_request_instance.dataset_access_model_request.access_model.data_access_model.validation_unit
-            )
-            approval_date = data_request_instance.dataset_access_model_request.modified
+        dam_type = (
+            data_request_instance.dataset_access_model_request.access_model.data_access_model.type
+        )
+        if dam_type != "OPEN":
+            # Check if the dataset model request is APPROVED
+            if dataset_access_request_status == "APPROVED":
+                validation = (
+                    data_request_instance.dataset_access_model_request.access_model.data_access_model.validation
+                )
+                validation_unit = (
+                    data_request_instance.dataset_access_model_request.access_model.data_access_model.validation_unit
+                )
+                approval_date = data_request_instance.dataset_access_model_request.modified
 
-            if validation_unit == ValidationUnits.DAY:
-                validation_deadline = approval_date + datetime.timedelta(
-                    days=validation
-                )
-                if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
-                    raise GraphQLError("The data access model is no longer valid.")
-            elif validation_unit == ValidationUnits.WEEK:
-                validation_deadline = approval_date + datetime.timedelta(
-                    weeks=validation
-                )
-                if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
-                    raise GraphQLError("The data access model is no longer valid.")
-            elif validation_unit == ValidationUnits.MONTH:
-                validation_deadline = approval_date + datetime.timedelta(
-                    days=(30 * validation)
-                )
-                if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
-                    raise GraphQLError("The data access model is no longer valid.")
-            elif validation_unit == ValidationUnits.YEAR:
-                validation_deadline = approval_date + datetime.timedelta(
-                    days=(365 * validation)
-                )
-                if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
-                    raise GraphQLError("The data access model is no longer valid.")
+                if validation_unit == ValidationUnits.DAY:
+                    validation_deadline = approval_date + datetime.timedelta(
+                        days=validation
+                    )
+                    if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
+                        raise GraphQLError("The data access model is no longer valid.")
+                elif validation_unit == ValidationUnits.WEEK:
+                    validation_deadline = approval_date + datetime.timedelta(
+                        weeks=validation
+                    )
+                    if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
+                        raise GraphQLError("The data access model is no longer valid.")
+                elif validation_unit == ValidationUnits.MONTH:
+                    validation_deadline = approval_date + datetime.timedelta(
+                        days=(30 * validation)
+                    )
+                    if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
+                        raise GraphQLError("The data access model is no longer valid.")
+                elif validation_unit == ValidationUnits.YEAR:
+                    validation_deadline = approval_date + datetime.timedelta(
+                        days=(365 * validation)
+                    )
+                    if datetime.datetime.now(datetime.timezone.utc) > validation_deadline:
+                        raise GraphQLError("The data access model is no longer valid.")
+                return func(*args, **kwargs)
+        else:
             return func(*args, **kwargs)
-
     return inner
