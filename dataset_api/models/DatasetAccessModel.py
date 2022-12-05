d7c8b60a620bae7024300e741989af4b874a8444
Deepthi Chand <eopoxf@gmail.com>
2022-12-05 17:27:45 +0530

    add title to dataset access model

--- dataset_api/models/DatasetAccessModel.py
+++ dataset_api/models/DatasetAccessModel.py
@@ -5,6 +5,7 @@ from dataset_api.models.Dataset import Dataset
 
 
 class DatasetAccessModel(models.Model):
+    title = models.CharField(max_length=100)
     issued = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     data_access_model = models.ForeignKey(DataAccessModel, on_delete=models.CASCADE)
