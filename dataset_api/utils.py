efbd856b013cc864ab127f5cc0466dce4ec30ab2
Abhinav <a.kumar2102@gmail.com>
2022-12-07 21:03:35 +0530

    fix json keep column  parent deletion

--- dataset_api/utils.py
+++ dataset_api/utils.py
@@ -74,24 +74,23 @@ def idp_make_cache_key(group, window, rate, value, methods):
 #     return d
 
 def json_keep_column(data, cols):
-    child_keys_list = []
     try:
         
-        def get_child_keys(d):
+        def get_child_keys(d, child_keys_list):
             if isinstance(d,  dict):
                 for key in list(d.keys()):
                     child_keys_list.append(key)
                     if isinstance(d[key], dict):
-                        get_child_keys(d[key])  
+                        get_child_keys(d[key], child_keys_list)  
             if isinstance(d,  list):
                 for each in d:
                     if isinstance(each,  dict):
-                        get_child_keys(each)                          
+                        get_child_keys(each, child_keys_list)                          
 
         def remove_a_key(d, remove_key):
             for key in list(d.keys()):
                 child_keys_list = []
-                get_child_keys(d[key])
+                get_child_keys(d[key], child_keys_list)
                 print ('--------------', child_keys_list)
                 if key not in remove_key and not any([ item in remove_key for item in child_keys_list]):
                     del d[key]
@@ -110,7 +109,6 @@ def json_keep_column(data, cols):
         return keep_col(data, cols)
     except:
         return data
-
 
 
 def clone_object(obj, attrs={}):
