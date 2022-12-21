8196bb01370825483aaa2bf7f9728d736b63b4a4
ArcD7 <pokemongochamp47@gmail.com>
2022-12-21 20:56:36 +0530

    Migrations

--- dataset_api/models/Sector.py
+++ dataset_api/models/Sector.py
@@ -6,4 +6,4 @@ class Sector(models.Model):
     name = models.CharField(max_length=75)
     description = models.CharField(max_length=100, null=False)
     highlights = ArrayField(models.CharField(max_length=100, blank=True, null=True), blank=True, null=True)
-    official_id = models.CharField(max_length=100, null=False, unique=True)
+    official_id = models.CharField(max_length=100, null=True, unique=False, default='')
