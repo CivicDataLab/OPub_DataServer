5a140594284cbac650bc8b901d4f81969a750ff5
ArcD7 <pokemongochamp47@gmail.com>
2023-01-30 11:08:21 +0530

    Increased sector descp max length to 1000

--- dataset_api/models/Sector.py
+++ dataset_api/models/Sector.py
@@ -4,6 +4,6 @@ from django.contrib.postgres.fields import ArrayField
 
 class Sector(models.Model):
     name = models.CharField(max_length=75)
-    description = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=1000, null=False)
     highlights = ArrayField(models.CharField(max_length=100, blank=True, null=True), blank=True, null=True)
     official_id = models.CharField(max_length=100, null=True, unique=False, default='')
