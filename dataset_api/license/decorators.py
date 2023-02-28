e8beb6f64a56a80f065e8ab99f2fde9bba22f17f
Archit Dubey <pokemongochamp47@gmail.com>
2023-02-28 15:57:58 +0530

    Added auth checks for policy flow.

--- dataset_api/license/decorators.py
+++ dataset_api/license/decorators.py
@@ -5,27 +5,28 @@ from graphql import GraphQLError
 from dataset_api.decorators import request_to_server
 from dataset_api.models.License import License
 
+def check_license_role(action):
+    def accept_func(func):
+        def inner(*args, **kwargs):
+            user_token = args[1].context.META.get("HTTP_AUTHORIZATION")
+            org_id = args[1].context.META.get("HTTP_ORGANIZATION")
+            body = json.dumps(
+                {
+                    "access_token": user_token,
+                    "access_req": action,
+                    "access_org_id": org_id,
+                    "access_data_id": "",
+                }
+            )
+            response_json = request_to_server(body, "check_user_access")
+            if not response_json["Success"]:
+                raise GraphQLError(response_json["error_description"])
+            if response_json["access_allowed"]:
+                kwargs["role"] = response_json["role"]
+                return func(*args, **kwargs)
 
-def check_license_role(func):
-    def inner(*args, **kwargs):
-        user_token = args[1].context.META.get("HTTP_AUTHORIZATION")
-        org_id = args[1].context.META.get("HTTP_ORGANIZATION")
-        body = json.dumps(
-            {
-                "access_token": user_token,
-                "access_req": "create_license",
-                "access_org_id": org_id,
-                "access_data_id": "",
-            }
-        )
-        response_json = request_to_server(body, "check_user_access")
-        if not response_json["Success"]:
-            raise GraphQLError(response_json["error_description"])
-        if response_json["access_allowed"]:
-            kwargs["role"] = response_json["role"]
-            return func(*args, **kwargs)
-
-    return inner
+        return inner
+    return accept_func
 
 
 def auth_query_license(action):
