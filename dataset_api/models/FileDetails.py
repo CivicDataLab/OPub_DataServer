7a27fd4f04a70c94a3395b99b536c8fc7429bb4b
Abhinav <a.kumar2102@gmail.com>
2023-03-03 17:46:48 +0530

    changes for nic_obj_storage

--- dataset_api/models/FileDetails.py
+++ dataset_api/models/FileDetails.py
@@ -13,6 +13,6 @@ class FileDetails(models.Model):
         Resource, on_delete=models.CASCADE, primary_key=True
     )
     format = models.CharField(max_length=50)
-    file = models.FileField(storage=fs, upload_to=_resource_directory_path, blank=True, max_length=1000)
+    file = models.FileField(upload_to=_resource_directory_path, blank=True, max_length=1000)
     remote_url = models.URLField(blank=True)
     source_file_name = models.CharField(max_length=100)
--- dataset_api/resource_file.py
+++ dataset_api/resource_file.py
@@ -8,10 +8,10 @@ from dataset_api.models import Resource
 
 def download(request, resource_id):
     resource = Resource.objects.get(pk=resource_id)
-    file_path = resource.filedetails.file.path
+    file_path = resource.filedetails.file.name
     if len(file_path):
         mime_type = magic.from_file(file_path, mime=True)
-        response = HttpResponse(resource.filedetails.file, content_type=mime_type)
+        response = "csv" #HttpResponse(resource.filedetails.file, content_type=mime_type)
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
     else:
         response = HttpResponse("file doesnt exist", content_type='text/plain')
