8e8997a85f1e481acf052841ba946331ef4a5a79
thev <the.v007@icloud.com>
2023-03-07 17:19:19 +0530

    update sector added

--- dataset_api/sector_schema.py
+++ dataset_api/sector_schema.py
@@ -76,6 +76,7 @@ class SectorInput(graphene.InputObjectType):
     highlights = graphene.List(of_type=graphene.String)
     official_id = graphene.String(required=True)
     # organization = graphene.String()
+    parent_id = graphene.ID(required=False)
 
 
 class CreateSector(Output, graphene.Mutation):
@@ -91,6 +92,7 @@ class CreateSector(Output, graphene.Mutation):
             description=sector_data.description,
             highlights=sector_data.highlights,
             official_id=sector_data.official_id,
+            parent_id=sector_data.parent_id,
         )
         sector_instance.save()
         return CreateSector(sector=sector_instance)
@@ -111,7 +113,9 @@ class UpdateSector(Output, graphene.Mutation):
         if sector_data.highlights:
             sector_instance.highlights = sector_data.highlights
         if sector_data.official_id:
-            sector_instance.official_id = sector_data.official_id            
+            sector_instance.official_id = sector_data.official_id
+        if sector_data.parent_id:
+            sector_instance.parent_id = sector_data.parent_id
         sector_instance.save()
         return UpdateSector(sector=sector_instance)
     
