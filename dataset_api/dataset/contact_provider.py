21102db40f8f09c160ec79f6b0ebe8fb72185222
Ubuntu <ubuntu@ip-172-31-30-64.ap-south-1.compute.internal>
2023-03-12 08:54:27 +0000

    change in contact_provider api

--- dataset_api/dataset/contact_provider.py
+++ dataset_api/dataset/contact_provider.py
@@ -1,10 +1,12 @@
 import json
 from ..email_utils import contact_provider_notif
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
 
 '''
 Trigger mail for contact_provider
 '''
-
+@csrf_exempt
 def contact_provider(request):
     
     post_data = json.loads(request.body.decode("utf-8"))
@@ -12,5 +14,8 @@ def contact_provider(request):
     # Send email notification to the user for subscribing to dataset.
     try:
         contact_provider_notif(post_data)
+        return JsonResponse({"Success": True}, safe=False)
     except Exception as e:
         print(str(e))
+
+    return JsonResponse({"Success": False}, safe=False)
