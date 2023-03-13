c709c5f379b9290471a814ab5e390360b19030b3
Abhinav <a.kumar2102@gmail.com>
2023-03-13 09:23:45 +0530

    change in policy and license download for s3

--- dataset_api/license_file.py
+++ dataset_api/license_file.py
@@ -9,9 +9,9 @@ from dataset_api.models.License import License
 
 def download(request, license_id):
     license = License.objects.get(pk=license_id)
-    file_path = license.file.path
+    file_path = license.file.name
     if len(file_path):
-        mime_type = magic.from_file(file_path, mime=True)
+        mime_type = magic.from_file(license.file, mime=True)
         response = HttpResponse(license.file, content_type=mime_type)
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
     else:
--- dataset_api/policy/policy_file.py
+++ dataset_api/policy/policy_file.py
@@ -9,9 +9,9 @@ from dataset_api.models.Policy import Policy
 
 def download(request, policy_id):
     policy = Policy.objects.get(pk=policy_id)
-    file_path = policy.file.path
+    file_path = policy.file.name
     if len(file_path):
-        mime_type = magic.from_file(file_path, mime=True)
+        mime_type = magic.from_buffer(policy.file, mime=True)
         response = HttpResponse(policy.file, content_type=mime_type)
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
     else:
