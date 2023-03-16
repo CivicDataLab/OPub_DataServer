d25694d80cc990b935b2f03c48c1c79dccd711b4
Abhinav <a.kumar2102@gmail.com>
2023-03-16 13:05:53 +0530

    change in policy and license download for s3

--- dataset_api/license_file.py
+++ dataset_api/license_file.py
@@ -5,13 +5,13 @@ import magic
 from django.http import HttpResponse
 
 from dataset_api.models.License import License
-
+from django.core.files.storage import default_storage
 
 def download(request, license_id):
     license = License.objects.get(pk=license_id)
-    file_path = license.file.name
-    if len(file_path):
-        mime_type = magic.from_file(license.file, mime=True)
+    file_path = "/" + license.file.name
+    if default_storage.exists(file_path):
+        mime_type = magic.from_buffer(license.file.read(), mime=True)
         response = HttpResponse(license.file, content_type=mime_type)
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
     else:
--- dataset_api/policy/policy_file.py
+++ dataset_api/policy/policy_file.py
@@ -5,13 +5,13 @@ import magic
 from django.http import HttpResponse
 
 from dataset_api.models.Policy import Policy
-
+from django.core.files.storage import default_storage
 
 def download(request, policy_id):
     policy = Policy.objects.get(pk=policy_id)
-    file_path = policy.file.name
-    if len(file_path):
-        mime_type = magic.from_buffer(policy.file, mime=True)
+    file_path = "/" + policy.file.name
+    if default_storage.exists(file_path):
+        mime_type = magic.from_buffer(policy.file.read(), mime=True)
         response = HttpResponse(policy.file, content_type=mime_type)
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
     else:
